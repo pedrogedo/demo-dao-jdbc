2a569e88d1417476913bbbde1f69fe3f69ac5084
Pedro Henrique Rosendo da Silva <pedro.ads@otlook.com>
2023-04-24 21:14:33 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -13,6 +14,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("==== TEST 1: seller findById ====");
 		Seller seller = sellerDao.finById(3);
@@ -27,15 +29,23 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
-		System.out.println("\n==== TEST 4: seller insert ====");
-		Seller newSeller = new Seller(null, "Biel Rosendo", "bielzingedo@gmail.com", new Date(), 1500.00, department);
-		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New id = " + newSeller.getId());
+//		System.out.println("\n==== TEST 4: seller insert ====");
+//		Seller newSeller = new Seller(null, "Biel Rosendo", "bielzingedo@gmail.com", new Date(), 1500.00, department);
+//		sellerDao.insert(newSeller);
+//		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		System.out.println("\n==== TEST 5: seller update ====");
 		seller = sellerDao.finById(1);
 		seller.setName("Maria Joaquina");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n==== TEST 6: seller delete ====");
+		System.out.print("Enter id for delete: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -101,10 +101,22 @@ public class SellerDaoJDBC implements SellerDao {
 		
 	}
 
+	/**
+	 * Este método deleta um Seller pelo seu ID
+	 */
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = connection.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			st.setInt(1, id);
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		finally {
+			DB.closeStatment(st);
+		}
 	}
 
 	/**
